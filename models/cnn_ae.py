ee25e976b68979cd621be8fe99c0b1a8c4494d60
Nathaniel Driggs <nathanieldriggs@gmail.com>
2024-10-10 11:23:02 -0600

    fixed model internal dimensions so everything matches up

--- models/cnn_ae.py
+++ models/cnn_ae.py
@@ -5,12 +5,12 @@ import torchvision.models as models
 class UpsampleBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(UpsampleBlock, self).__init__()
-        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding='same', output_padding=1)
+        self.conv = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1, output_padding=1)
         self.batchnorm = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU()
 
     def forward(self, x):
-        return self.relu(self.conv(x))
+        return self.relu(self.batchnorm(self.conv(x)))
 
 class CNNAutoEncoder(nn.Module):
     def __init__(self, in_channels):
@@ -19,8 +19,6 @@ class CNNAutoEncoder(nn.Module):
         # Base model (MobileNetV2)
         self.base_model = models.mobilenet_v2(weights=None)
         self.base_model.features[0][0] = nn.Conv2d(in_channels, 32, kernel_size=3, stride=2, padding=1, bias=False)
-
-        print('num layers:', len(self.base_model.features))
         
         # Encoder (down_stack)
         self.down_stack = nn.ModuleList([
@@ -33,15 +31,15 @@ class CNNAutoEncoder(nn.Module):
         
         # Decoder (up_stack)
         self.up_stack = nn.ModuleList([
-            UpsampleBlock(1280, 512),
-            UpsampleBlock(512, 256),
-            UpsampleBlock(256, 128),
-            UpsampleBlock(128, 64)
+            UpsampleBlock(160, 96),
+            UpsampleBlock(96 + 96, 32),
+            UpsampleBlock(32 + 32, 24),
+            UpsampleBlock(24 + 24, 16)
         ])
         
         # Final layers
-        self.final_upsample = nn.ConvTranspose2d(64, 32, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.final_conv = nn.Conv2d(32, 1, kernel_size=1, padding='same')
+        self.final_upsample = nn.ConvTranspose2d(16+16, 1, kernel_size=3, stride=2, padding=1, output_padding=1)
+        self.final_conv = nn.Conv2d(1, 1, kernel_size=1, padding='same')
         self.sigmoid = nn.Sigmoid()
 
     def forward(self, x):
@@ -50,16 +48,11 @@ class CNNAutoEncoder(nn.Module):
         for down in self.down_stack:
             x = down(x)
             skips.append(x)
-
-        print(x.shape)
         
         skips = skips[::-1][1:]  # Reverse and remove last skip connection
         
         # Upsampling
-        i = 1
         for up, skip in zip(self.up_stack, skips):
-            print(i)
-            i += 1
             x = up(x)
             x = torch.cat([x, skip], dim=1)
         
